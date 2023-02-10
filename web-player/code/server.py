53730786fea8c628703f1f86ef6a37d0de4ad720
toulim <toulim0525@ewhain.net>
2023-02-10 14:42:33 +0900

    flask 파일 업로드

--- web-player/code/server.py
+++ web-player/code/server.py
@@ -1,11 +1,27 @@
-from flask import Flask
-from flask.templating import render_template
+from flask import Flask, request, url_for, render_template
+from werkzeug.utils import secure_filename
+import os
+import time
 
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'C:/Users/82108/Desktop/졸프/24_project/web-player/code/upload'
+
 
 @app.route('/')
-def index():
+def render():
     return render_template('player.html')
 
-app.run(host='127.0.0.1')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], 'content'))
+        return render_template('player.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+app.run(host='127.0.0.1', debug=True)
