8a557712094f2266fac68ecf842e5ba1e0e42c08
chaemin <87525734+cmlim0070@users.noreply.github.com>
2023-05-01 21:31:46 +0900

    테스트

--- web-player/code/server.py
+++ web-player/code/server.py
@@ -1,33 +1,33 @@
-import json
+# import json
 
 from sklearn import preprocessing
 import app
-import io
-import logging
+# import io
+# import logging
 import os
-import threading
+# import threading
 import subprocess
-import sys
+# import sys
 
-from confugue import Configuration
+# from confugue import Configuration
 import flask
-from flask_cors import CORS
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from note_seq.protobuf.music_pb2 import NoteSequence
-from museflow.note_sequence_utils import normalize_tempo
-import numpy as np
-import tensorflow as tf
-import werkzeug.exceptions
-from werkzeug.middleware.proxy_fix import ProxyFix
+# from flask_cors import CORS
+# from flask_limiter import Limiter
+# from flask_limiter.util import get_remote_address
+# from note_seq.protobuf.music_pb2 import NoteSequence
+# from museflow.note_sequence_utils import normalize_tempo
+# import numpy as np
+# import tensorflow as tf
+# import werkzeug.exceptions
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
-from groove2groove.io import NoteSequencePipeline
-from groove2groove.models import roll2seq_style_transfer
+# from groove2groove.io import NoteSequencePipeline
+# from groove2groove.models import roll2seq_style_transfer
 
 from flask import Flask, request, url_for, render_template
 from werkzeug.utils import secure_filename
 import os
-import time
+# import time
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -41,25 +41,25 @@ def render():
     return render_template('player.html')
 
 
-if 'STATIC_FOLDER' in app.config:
-    app.static_folder = app.config['STATIC_FOLDER']
-    app.static_url_path = '/'
+# if 'STATIC_FOLDER' in app.config:
+#     app.static_folder = app.config['STATIC_FOLDER']
+#     app.static_url_path = '/'
 
 
-app.wsgi_app = ProxyFix(app.wsgi_app, **app.config.get('PROXY_FIX', {}))
-limiter = Limiter(app, key_func=get_remote_address,
-                  headers_enabled=True, **app.config.get('LIMITER', {}))
-CORS(app, **app.config.get('CORS', {}))
+# app.wsgi_app = ProxyFix(app.wsgi_app, **app.config.get('PROXY_FIX', {}))
+# limiter = Limiter(app, key_func=get_remote_address,
+#                   headers_enabled=True, **app.config.get('LIMITER', {}))
+# CORS(app, **app.config.get('CORS', {}))
 
-logging.getLogger('tensorflow').handlers.clear()
+# logging.getLogger('tensorflow').handlers.clear()
 
-models = {}
-model_graphs = {}
-tf_lock = threading.Lock()
+# models = {}
+# model_graphs = {}
+# tf_lock = threading.Lock()
 
 
 @ app.route('/output/', methods=['GET', 'POST'])
-@ limiter.limit(app.config.get('MODEL_RATE_LIMIT', None))
+# @ limiter.limit(app.config.get('MODEL_RATE_LIMIT', None))
 def run_model():
     try:
         files = flask.request.files
@@ -110,4 +110,4 @@ def get_tempo(mid):
 
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', debug=True)
-    app.run()
+    app.run
